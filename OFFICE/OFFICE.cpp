1dd9b3aaf07a6038778092296f1fd0176fd0c349
Rohita Lal <rohitalal2002@gmail.com>
2022-08-11 12:53:31 +0530

    files added

---
+++ OFFICE/OFFICE.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,x,y,c;
+	cin>>T;
+	for(int i=1;i<=T;i++)
+	{
+	    cin>>x>>y;
+	    {
+	        c=4*x+y;
+	        cout<<c;
+	    }
+	    cout<<"\n";
+	}
+	return 0;
+}
